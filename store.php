20e75d52d630152e6943bc5c5f2b7ca615067fd3
kathith kumar sahu <106585291+coder-kks@users.noreply.github.com>
2022-09-30 20:53:04 +0530

    sql

--- store.php
+++ store.php
@@ -3,4 +3,29 @@ $myfile = fopen("info.txt", "w") or die("Unable to open file!");
 $txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP: " . $_SERVER["REMOTE_ADDR"];
 fwrite($myfile, $txt);
 fclose($myfile);
-?>
+
+
+
+$mysqli = new mysqli("localhost","root","","dbname");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+
+$lat = $_GET["lat"];
+$long = $_GET["long"];
+$ip = $_SERVER["REMOTE_ADDR"];
+	$sql = "INSERT INTO users (lat,long,ip)
+VALUES ('$lat', '$long', '$ip')";
+
+if ($mysqli->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $mysqli->error;
+}
+
+$conn->close();
+
